8aba7ae5d660262ef6c1955581f7f4f9a65bb2af
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:02:48 +0530

    Update D_HH_MM_SS.py

--- Basic Code/D_HH_MM_SS.py
+++ Basic Code/D_HH_MM_SS.py
@@ -1,3 +1,9 @@
+# In this exercise you will reverse the process described in the previous exercise.
+# Develop a program that begins by reading a number of seconds from the user.
+# Then your program should display the equivalent amount of time in the form
+# D:HH:MM:SS, where D, HH, MM, and SS represent days, hours, minutes and sec-
+# onds respectively. The hours, minutes and seconds should all be formatted so that
+# they occupy exactly two digits, with a leading 0 displayed if necessary.
 seconds=int(input("Enter number of seconds : "))
 days= seconds//(24*3600)
 seconds= seconds%(24*3600)
