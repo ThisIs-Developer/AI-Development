ed7c1e901552f72aec8e7b8326a1738240d3ec4b
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:05:35 +0530

    Update Pressure.py

--- Basic Code/Pressure.py
+++ Basic Code/Pressure.py
@@ -1,3 +1,7 @@
+# In this exercise you will create a program that reads a pressure from the user in kilo-
+# pascals. Once the pressure has been read your program should report the equivalent
+# pressure in pounds per square inch, millimeters of mercury and atmospheres. Use
+# your research skills to determine the conversion factors between these units.
 kilo_pascals= float(input("Enter pressure in kilo_pascals : "))
 psi= kilo_pascals/6.89475729
 mmhg = kilo_pascals*7.50062
