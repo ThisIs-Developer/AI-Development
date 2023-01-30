1a3b5b5811eb5a9e12c98884cae33156094ca046
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 00:57:20 +0530

    Update Interest.py

--- Basic Code/Interest.py
+++ Basic Code/Interest.py
@@ -1,3 +1,10 @@
+# Pretend that you have just opened a new savings account that earns 4 percent
+# interest per year. The interest that you earn is paid at the end of the year, and is
+# added to the balance of the savings account. Write a program that begins by reading
+# the amount of money deposited into the account from the user. Then your program
+# should compute and display the amount in the savings account after 1, 2, and 3
+# years. Display each amount so that it is rounded to 2 decimal places.
+
 Deposite=float(input("Enter the Amount = "))
 interest=0.04
 year=1
