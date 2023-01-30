3387ecb4f51a6377d5e404bf1a237d5a04623039
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:01:52 +0530

    Update Volume_sphere.py

--- Basic Code/Volume_sphere.py
+++ Basic Code/Volume_sphere.py
@@ -1,3 +1,10 @@
+# Write a program that begins by reading a radius, r , from the user. The program will
+# continue by computing and displaying the area of a circle with radius r and the
+# volume of a sphere with radius r . Use the pi constant in the math module in your
+# calculations.
+# Hint: The area of a circle is computed using the formula area = π r 2 .
+# The volume of a sphere is computed using the formula volume =
+# 4/3 πr3
 import math
 from math import pi
 radius = float(input ("Enter the radius(r) : "))
