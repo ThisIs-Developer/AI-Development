7c830e2f82d1e90f21a591d31faf2f1042b37778
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:04:37 +0530

    Update Sum.py

--- Basic Code/Sum.py
+++ Basic Code/Sum.py
@@ -1,3 +1,6 @@
+# Develop a program that reads a four-digit integer from the user and displays the sum
+# of the digits in the number. For example, if the user enters 3141 then your program
+# should display 3 + 1 + 4 + 1 = 9.
 num= int(input("Input numbers(only 4 dighit): "))
 no= num//1000
 no1= (num-no*1000)//100
