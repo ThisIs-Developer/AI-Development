899483f539d0b88c59c4e283d515fbec1ca2c2cc
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 00:53:58 +0530

    Update Input_print.py

--- Basic Code/Input_print.py
+++ Basic Code/Input_print.py
@@ -1,2 +1,4 @@
+# Write a program that asks the user to enter his or her name. The program
+# should respond with a message that says hello to the user, using his or her name.
 NAME=str(input("Enter your name: "))
 print("Hello",NAME)
