5657b1062c7a5a960adb60b102311ee5aacd845d
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:00:40 +0530

    Update Height1.py

--- Basic Code/Height1.py
+++ Basic Code/Height1.py
@@ -1,3 +1,8 @@
+# Many people think about their height in feet and inches, even in some countries that
+# primarily use the metric system. Write a program that reads a number of feet from
+# the user, followed by a number of inches. Once these values are read, your program
+# should compute and display the equivalent number of centimeters.
+# Hint: One foot is 12 inches. One inch is 2.54 centimeters.
 ft = int(input("Enter your hieght in Feet : "))
 inch = int(input("Enter your hieght in Inches : "))
 inch+= (ft*12)
