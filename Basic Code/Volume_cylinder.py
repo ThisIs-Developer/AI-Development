aa22d949fbc9642835590fa73eb463d45721f892
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:02:15 +0530

    Update Volume_cylinder.py

--- Basic Code/Volume_cylinder.py
+++ Basic Code/Volume_cylinder.py
@@ -1,3 +1,7 @@
+# The volume of a cylinder can be computed by multiplying the area of its circular
+# base by its height. Write a program that reads the radius of the cylinder, along with
+# its height, from the user and computes its volume. Display the result rounded to one
+# decimal place.
 import math
 from math import pi
 radius = float(input ("Enter the radius(r) : "))
