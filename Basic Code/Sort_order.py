505d1dbde760460e9a9f82c633b7fa58f2d68852
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:05:06 +0530

    Update Sort_order.py

--- Basic Code/Sort_order.py
+++ Basic Code/Sort_order.py
@@ -1,3 +1,7 @@
+# Create a program that reads three integers from the user and displays them in sorted
+# order (from smallest to largest). Use the min and max functions to find the smallest
+# and largest values. The middle value can be found by computing the sum of all three
+# values, and then subtracting the minimum value and the maximum value.
 x= int(input("Input first number: "))
 y= int(input("Input second number: "))
 z= int(input("Input third number: "))
