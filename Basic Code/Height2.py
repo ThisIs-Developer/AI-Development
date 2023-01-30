ddf3c379fc1b48fa1f12daafb8d84cfea3b6db0a
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:01:06 +0530

    Update Height2.py

--- Basic Code/Height2.py
+++ Basic Code/Height2.py
@@ -1,3 +1,7 @@
+# In this exercise, you will create a program that begins by reading a measurement
+# in feet from the user. Then your program should display the equivalent distance in
+# inches, yards and miles. Use the Internet to look up the necessary conversion factors
+# if you don’t have them memorized.
 ft = int(input("Enter the distance in feet : "))
 inches = ft * 12
 yards = ft / 3.0
