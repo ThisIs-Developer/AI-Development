5410d7f3849db7b88df2d9118a5f259060de9f1b
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 01:03:37 +0530

    Update Time.py

--- Basic Code/Time.py
+++ Basic Code/Time.py
@@ -1,3 +1,8 @@
+# Python includes a library of functions for working with time, including a function
+# called asctime in the time module. It reads the current time from the com-
+# puter’s internal clock and returns it in a human-readable format. Write a program
+# that displays the current time and date. Your program will not require any input
+from the user.
 import time
 now = time.asctime()
 print(now)
