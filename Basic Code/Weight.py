c97cff50e362fd3cbd64520ff366cc3802629c41
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 00:56:40 +0530

    Update Weight.py

--- Basic Code/Weight.py
+++ Basic Code/Weight.py
@@ -1,3 +1,8 @@
+# An online retailer sells two products: widgets and gizmos. Each widget weighs 75
+# grams. Each gizmo weighs 112 grams. Write a program that reads the number of
+# widgets and the number of gizmos in an order from the user. Then your program
+# should compute and display the total weight of the order.
+
 #Widgets=75 grams
 #Gizmos=112 grams
 Widgets=int(input("Enter your number of Widgets: "))
