5c8b784f5e494f1b6448cf6e80aeaaf92b5d9e5c
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 00:54:55 +0530

    Update Area_field.py

--- Basic Code/Area_field.py
+++ Basic Code/Area_field.py
@@ -1,3 +1,6 @@
+Create a program that reads the length and width of a farmer’s field from the user in
+feet. Display the area of the field in acres. Hint: There are 43,560 square feet in an
+acre.
 width=float(input("Enter the width (feet): "))
 length=float(input("Enter the length (feet): "))
 squarefeet= 43560 
