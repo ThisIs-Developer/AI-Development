16e6285e423dba714a731fd3ec240a1bd886c077
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-02-01 01:16:53 +0530

    Update Temperature.py

--- Basic Code/Temperature.py
+++ Basic Code/Temperature.py
@@ -1,4 +1,4 @@
-# 2.	Write a Python program to convert temperatures to and from celsius, fahrenheit.
+#	Write a Python program to convert temperatures to and from celsius, fahrenheit.
 celsius=float(input("Enter Temperature(celsius): "))
 fahrenheit = (celsius*(9/5))+32
 print("Temperature in Fahrenheit = %.2ff"%fahrenheit)
