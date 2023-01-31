8c2d276d85bbb1e33747c66e588185425c3a94db
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-01-31 15:31:11 +0530

    Add files via upload

---
+++ Basic Code/relational_operations.py
@@ -0,0 +1,9 @@
+# Write a python program to perform all relational operations on 2 given numbers.
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+print(f'{num1} < {num2} = {num1 < num2}')
+print(f'{num1} > {num2} = {num1 > num2}')
+print(f'{num1} <= {num2} = {num1 <= num2}')
+print(f'{num1} >= {num2} = {num1 >= num2}')
+print(f'{num1} == {num2}: {num1 == num2}')
+print(f'{num1} != {num2}: {num1 != num2}')
