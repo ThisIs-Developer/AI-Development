e5b03995145fc9d054588da4ffd556923bcb586f
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-07-11 23:30:29 +0530

    Delete List directory

--- List/Empty_list.py
+++
@@ -1,7 +0,0 @@
-#Write a python program to accept N integers from the user and store it into a list and then display them.
-myList = [] #python 2.7
-N = int(input('Enter number of values to be inserted: '))
-print(f'Enter {N} integers: ')
-for i in range(N):
-  myList.append(int(input()))
-print(f'Accepted Integers = {myList}')
